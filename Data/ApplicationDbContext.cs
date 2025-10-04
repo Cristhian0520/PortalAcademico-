d1608ec9001f83dfce3d718016c652e5d1ed3c4e
Cristhian0520 <cristhian200502@gmail.com>
2025-10-04 14:11:44 -0500

    base

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlataformaAcademicaApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
